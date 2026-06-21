c3599d13b4f335a7ea9f226f0d5d539b76cf126a
Soar Qin <soarchin@gmail.com>
2026-06-22 00:35:33 +0800

    fix: fmg repack script

--- update_fmg.bat
+++ update_fmg.bat
@@ -1,2 +1,5 @@
-@tools\fmgcarry.exe origin\engUS\item_dlc02 mod\engUS\item_dlc02 origin\zhoCN\item_dlc02 item
-@tools\fmgcarry.exe origin\engUS\menu_dlc02 mod\engUS\menu_dlc02 origin\zhoCN\menu_dlc02 menu
+@echo off
+copy /y origin\zhoCN\item_dlc02\*.* mod\zhoCN\item_dlc02\
+copy /y origin\zhoCN\menu_dlc02\*.* mod\zhoCN\menu_dlc02\
+tools\fmgcarry.exe origin\engUS\item_dlc02 mod\engUS\item_dlc02 mod\zhoCN\item_dlc02 item
+tools\fmgcarry.exe origin\engUS\menu_dlc02 mod\engUS\menu_dlc02 mod\zhoCN\menu_dlc02 menu
